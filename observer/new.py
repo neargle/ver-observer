ae7dfdf28fbad4b9cea47191ebe8ca91d0d69df0
neargle <nearg1e.com@gmail.com>
2017-12-31 19:49:07 +0800

    add _git_exec, last_static function and default_info_result var

--- observer/new.py
+++ observer/new.py
@@ -7,28 +7,80 @@ import os
 import re
 import subprocess
 
+from .version import str2version
+
 
 class ProjectInfo(object):
     """Analyze git project infomation.
 
+    Create file like below :
+    {
+        "framework": "",
+        "alias": [],
+        "versions": [],
+        "fingerprint": {
+            "version": {
+                "filepath": "filehash"
+            }
+        }
+    }
+
     :param project_path: project_path.
     :param static_path: static_path.
     """
+
+    default_info_result = {
+        "framework": "",
+        "alias": [],
+        "versions": [],
+        "fingerprint": {}
+    }
 
     def __init__(self, target_project_path, static_path):
         self.target_project_path = os.path.realpath(target_project_path)
         if not os.path.isabs(static_path):
             static_path = os.path.join(self.target_project_path, static_path)
         self.static_path = static_path
+        self.version_lst = self.all_version()
+
+
+    def _git_exec(self, *args):
+        """git command.
+        """
+
+        master_program = 'git'
+        command_tup = (master_program, *args)
+        output = subprocess.check_output(command_tup, cwd=self.target_project_path)
+        return output.decode()
+
 
     def all_version(self):
         """get all version of project by 'git tag'.
         """
 
-        command = ('git', 'tag')
-        output = subprocess.check_output(command, cwd=self.target_project_path)
-        lines = output.decode().split('\n')
+        cmd_result = self._git_exec('tag')
+        lines = cmd_result.split('\n')
         # Some versions like Alpha(1.9a), Beta(1.9b) is out of consider.
         # Only return final version.
         pat = re.compile(r'^[1-9\.]+$')
-        return sorted([to_version(ver_) for ver_ in lines if pat.match(ver_)])
+        return sorted([str2version(ver_) for ver_ in lines if pat.match(ver_)]).reverse()
+
+
+    def last_static(self):
+        """all static files in last version.
+        """
+
+        static_files = []
+        for (root, _, filenames) in os.walk(self.static_path, followlinks=False):
+            for filename in filenames:
+                static_files.append(os.path.join(root, filename))
+        return static_files
+
+
+    def last_hash(self):
+        """
+        """
+
+        pass
+    
+
