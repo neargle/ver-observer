ecd8cb61bed2301c92257cfefe31b836884f8729
neargle <nearg1e.com@gmail.com>
2018-01-23 00:53:17 +0800

    add make_version function

--- observer/version.py
+++ observer/version.py
@@ -9,6 +9,7 @@ from distutils.version import LooseVersion
 from utils.common import remove_blank
 
 from utils.common import IS_PY3
+from utils.log import LOGGER as logger
 
 
 def str2version(version):
@@ -22,3 +23,30 @@ def str2version(version):
         return LooseVersion(remove_blank(version))
 
     return version
+
+
+def make_version(static_map, fingerprint):
+    """return version expression."""
+    version_compare_lst = []
+    key_lst = fingerprint.keys()
+    version_lst = sorted([str2version(ver_) for ver_ in key_lst], reverse=True)
+    last = version_lst[0]
+    # compare to last version
+    def _is_last():
+        last_fingerprint = fingerprint.get(last.vstring)
+        for path in last_fingerprint.keys():
+            hash_string = static_map.get(path, '')
+            if not hash_string:
+                continue
+            if hash_string == last_fingerprint.get(path):
+                yield True
+            else:
+                logger.noise(
+                    'path %s, real hash: %s, hash in last: %s',
+                    path, hash_string, last_fingerprint.get(path)
+                )
+                yield False
+
+    if all(_is_last()):
+        version_compare_lst.append((">=", last))
+    return version_compare_lst
