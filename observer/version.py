4f6ce0287fdf8a265a70e151f135b5b25a6fe8f7
neargle <nearg1e.com@gmail.com>
2018-01-23 12:31:23 +0800

    add make_operator function

--- observer/version.py
+++ observer/version.py
@@ -25,28 +25,36 @@ def str2version(version):
     return version
 
 
-def make_version(static_map, fingerprint):
+def make_operator(real_hash, fingerprint_hash):
+    """return a set of compare operator. like ('>=')."""
+    operators = set()
+    if real_hash:
+        if real_hash == fingerprint_hash:
+            operators.add('>=')
+        else:
+            if fingerprint_hash:
+                operators.add('!=')
+            else:
+                operators.add('>')
+    else:
+        if fingerprint_hash:
+            operators.add('<')
+        else:
+            operators.add('<=')
+    return operators
+
+
+def make_version(static_map, fingerprint_map):
     """return version expression."""
     version_compare_lst = []
-    key_lst = fingerprint.keys()
+    key_lst = fingerprint_map.keys()
     version_lst = sorted([str2version(ver_) for ver_ in key_lst], reverse=True)
     last = version_lst[0]
-    # compare to last version
-    def _is_last():
-        last_fingerprint = fingerprint.get(last.vstring)
-        for path in last_fingerprint.keys():
-            hash_string = static_map.get(path, '')
-            if not hash_string:
-                continue
-            if hash_string == last_fingerprint.get(path):
-                yield True
-            else:
-                logger.noise(
-                    'path %s, real hash: %s, hash in last: %s',
-                    path, hash_string, last_fingerprint.get(path)
-                )
-                yield False
-
-    if all(_is_last()):
-        version_compare_lst.append((">=", last))
+    # compare to each version fingerprint.
+    # make compare expressions. like [(">=", 'v2.3.3.3')]
+    for version in version_lst:
+        fingerprint = fingerprint_map.get(version.vstring)
+        for path, filehash in fingerprint:
+            real_hash = static_map.get(path)
+            operators = make_operator(real_hash, filehash)
     return version_compare_lst
